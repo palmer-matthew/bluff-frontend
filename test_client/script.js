b71b98a9630b3049bff791ba6b554445a0f36080
Matthew Palmer <palmer.matthew4567@gmail.com>
2023-10-20 22:56:41 -0500

    Update Test Client, Add Buttons to Test Backend Functions

--- test_client/script.js
+++ test_client/script.js
@@ -1,6 +1,58 @@
 import { Manager } from "socket.io-client";
 
-const SERVER="http://localhost:5050"
+function generateUsername(){
+   var name = 'Guest'
+   var number = Math.floor(Math.random() * 120000);
+   return name + String(number);
+}
+
+const SERVER="http://localhost:5050";
+const USERNAME=generateUsername();
 const manager = new Manager(SERVER);
 
 const socket = manager.socket("/");
+
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+
+button1.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const input = document.querySelector('#room-id');
+    var id = input.nodeValue;
+
+    localStorage.setItem('roomID', id);
+    
+    socket.emit("room:join" , {
+        roomID: id,
+        username: USERNAME
+    });
+
+    input.nodeValue = '';
+});
+
+button2.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    var id = localStorage.getItem('roomID');
+
+    socket.emit("room:join", {
+        roomID: id,
+        username: USERNAME
+    });
+
+    localStorage.removeItem('roomID');
+});
+
+button3.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+// // receiving an event
+// socket.on("foo", (value) => {
+//     // ...
+//   });
+  
+//   // sending an event
+//   socket.emit("bar", "abc");
