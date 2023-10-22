461e48f2e4f27e43f8000e1b19a78162f61ec23b
Matthew Palmer <palmer.matthew4567@gmail.com>
2023-10-21 19:59:56 -0500

    Continue Testing Room Functions

--- test_client/script.js
+++ test_client/script.js
@@ -1,9 +1,9 @@
 import { Manager } from "socket.io-client";
 
-function generateUsername(){
-   var name = 'Guest'
-   var number = Math.floor(Math.random() * 120000);
-   return name + String(number);
+const generateUsername = () => {
+    var name = 'Guest'
+    var number = Math.floor(Math.random() * 120000);
+    return name + String(number);
 }
 
 const SERVER="http://localhost:5050";
@@ -12,9 +12,26 @@ const manager = new Manager(SERVER);
 
 const socket = manager.socket("/");
 
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector('#button2');
-const button3 = document.querySelector('#button3');
+const greeting = document.querySelector('p');
+greeting.innerText = `Hello ${USERNAME}`;
+
+const button1 = document.querySelector('#join');
+const button2 = document.querySelector('#leave');
+const button3 = document.querySelector('#create');
+
+const sendJoinRoomEvent = (roomID, username) => {
+    socket.emit("room:join" , {
+        roomID,
+        username
+    });
+};
+
+const sendLeaveRoomEvent = (roomID, username) => {
+    socket.emit("room:leave", {
+        roomID,
+        username
+    });
+};
 
 button1.addEventListener('click', (event) => {
     event.preventDefault();
@@ -22,12 +39,10 @@ button1.addEventListener('click', (event) => {
     const input = document.querySelector('#room-id');
     var id = input.nodeValue;
 
+    console.log(id);
     localStorage.setItem('roomID', id);
     
-    socket.emit("room:join" , {
-        roomID: id,
-        username: USERNAME
-    });
+    sendJoinRoomEvent(id, USERNAME);
 
     input.nodeValue = '';
 });
@@ -37,16 +52,36 @@ button2.addEventListener('click', (event) => {
 
     var id = localStorage.getItem('roomID');
 
-    socket.emit("room:join", {
-        roomID: id,
-        username: USERNAME
-    });
+    sendLeaveRoomEvent(id, USERNAME);
 
     localStorage.removeItem('roomID');
 });
 
 button3.addEventListener('click', (event) => {
     event.preventDefault();
+
+    var searchParams = new URLSearchParams();
+    searchParams.append('name', USERNAME);
+    searchParams.append('player_count', 8);
+
+    fetch(SERVER, {
+        method: 'POST' ,
+        body: searchParams,
+    }).then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data);
+        
+        if(localStorage.getItem('roomID')){
+            localStorage.removeItem('roomID');
+        }
+        localStorage.setItem('roomID', data.roomID); 
+
+        sendJoinRoomEvent(data.roomID, USERNAME);
+        
+    }).catch (error => {
+        console.log(error);
+    })
 });
 
 // // receiving an event
